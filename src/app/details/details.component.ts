06e10ed667badbaf7b54777308dcb06e630106a7
dagmawi-asfaw <71550160+dagmawi-asfaw@users.noreply.github.com>
2025-01-09 03:51:53 +0300

    feat: add housing service

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,23 +1,50 @@
-import { Component,inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ActivatedRoute } from "@angular/router";
+import { HousingService } from "../housing.service";
+import { HousingLocation } from "../housing-location";
 
 @Component({
-  selector: 'app-details',
+  selector: "app-details",
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-       {{housingLocationId}}
-    </p>
+    <article>
+      <img
+        class="listing-photo"
+        [src]="housingLocation?.photo"
+        alt="Exterior photo of {{ housingLocation?.name }}"
+      />
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+        <p class="listing-location">
+          {{ housingLocation?.city }}, {{ housingLocation?.state }}
+        </p>
+        <section class="listing-features">
+          <h2 class="section-heading">About this location</h2>
+          <ul>
+            <li>Units Available: {{ housingLocation?.availableUnits }}</li>
+            <li>Does it have wifi? {{ housingLocation?.wifi }}</li>
+            <li>Does it have laundry? {{ housingLocation?.laundry }}</li>
+          </ul>
+        </section>
+        <section class="listing-apply">
+          <h2 class="section-heading">Apply now to live here.</h2>
+          <button class="primary" type="button">Apply</button>
+        </section>
+      </section>
+    </article>
   `,
-  styleUrls: ['./details.component.css']
+  styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
-  route:ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService: HousingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
   constructor() {
-     this.housingLocationId = Number(this.route.snapshot.params['id']);
+    const housingLocationId = Number(this.route.snapshot.params["id"]);
+    this.housingLocation =
+      this.housingService.getLocationById(housingLocationId);
   }
 }
